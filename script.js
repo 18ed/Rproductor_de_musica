574278c2cc32e212061730f8d2d4ffb3c54fe556
Nemesis Lugo <yonaibereduardolugosaa@gmail.com>
2024-03-20 17:22:40 -0400

    ;listo

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 
-const audioPlayer = document.getElementById('audioPlayer');
-const playButton = document.getElementById('playButton');
-const pauseButton = document.getElementById('pauseButton');
-const volumeSlider = document.getElementById('volumeSlider');
-const progressBar = document.getElementById('progressBar');
+// const audioPlayer = document.getElementById('audioPlayer');
+// const playButton = document.getElementById('playButton');
+// const pauseButton = document.getElementById('pauseButton');
+// const volumeSlider = document.getElementById('volumeSlider');
+// const progressBar = document.getElementById('progressBar');
 
-playButton.addEventListener('click', () => {
-    audioPlayer.play();
-  });
+// playButton.addEventListener('click', () => {
+//     audioPlayer.play();
+//   });
   
-  pauseButton.addEventListener('click', () => {
-    audioPlayer.pause();
-  });
+//   pauseButton.addEventListener('click', () => {
+//     audioPlayer.pause();
+//   });
   
-  volumeSlider.addEventListener('input', () => {
-    audioPlayer.volume = volumeSlider.value / 100;
-  });
+//   volumeSlider.addEventListener('input', () => {
+//     audioPlayer.volume = volumeSlider.value / 100;
+//   });
   
-  audioPlayer.addEventListener('timeupdate', () => {
-    const progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
-    progressBar.style.width = progress + '%';
-  });
+//   audioPlayer.addEventListener('timeupdate', () => {
+//     const progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+//     progressBar.style.width = progress + '%';
+//   });
+const tracklist = document.getElementById("tracklist")
+  
+  axios.get ("https://leonardoapi.onrender.com/songs")
+  .then((res) => {
+
+    res.data.songs.map ( (song) =>{
+
+      const div = document.createElement("div")
+      div.classList.add("h-16")
+      div.innerHTML = `
+        <img src="${song.path.front}" alt="" class="h=full">
+      `
+
+      tracklist.appendChild(div)
+    })
+
+
+  })
   
