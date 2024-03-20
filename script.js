0a4b419697c65125d4b20b4cb94a39bc958424b7
18ed <yonaibereduardolugosaa@gmail.com>
2024-03-20 12:48:07 -0400

    reproductor

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+const audioPlayer = document.getElementById('audioPlayer');
+const playButton = document.getElementById('playButton');
+const pauseButton = document.getElementById('pauseButton');
+const volumeSlider = document.getElementById('volumeSlider');
+const progressBar = document.getElementById('progressBar');
+
+playButton.addEventListener('click', () => {
+    audioPlayer.play();
+  });
+  
+  pauseButton.addEventListener('click', () => {
+    audioPlayer.pause();
+  });
+  
+  volumeSlider.addEventListener('input', () => {
+    audioPlayer.volume = volumeSlider.value / 100;
+  });
+  
+  audioPlayer.addEventListener('timeupdate', () => {
+    const progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+    progressBar.style.width = progress + '%';
+  });
+  
